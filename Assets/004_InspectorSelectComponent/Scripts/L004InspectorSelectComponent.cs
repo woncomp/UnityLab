d328492cd544c76a4da4c4d04cfb463bd8a70794
Joshua Zhang <woncomp@gmail.com>
2015-12-19 19:46:23 +0800

    Sample: Select component explicitly.

---
+++ Assets/004_InspectorSelectComponent/Scripts/L004InspectorSelectComponent.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class L004InspectorSelectComponent : MonoBehaviour {
+	
+	[ContextMenu("SelectMe")]
+	void SelectMe()
+	{
+		UnityEditor.Selection.objects = new Object[] { this };
+	}
+}
