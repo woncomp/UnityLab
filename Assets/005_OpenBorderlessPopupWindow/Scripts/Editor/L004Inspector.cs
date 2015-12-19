596db188569d414c3942e807bb7200162c9e3ced
Joshua Zhang <woncomp@gmail.com>
2015-12-19 20:04:32 +0800

    Sample: OpenBorderlessPopupWindow

---
+++ Assets/005_OpenBorderlessPopupWindow/Scripts/Editor/L004Inspector.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(L004Component))]
+public class L004Inspector : Editor {
+
+	public override void OnInspectorGUI()
+	{
+		if (GUILayout.Button("Open Window"))
+		{
+			L004Window.OpenWindow();
+		}
+	}
+}
---
+++ Assets/005_OpenBorderlessPopupWindow/Scripts/Editor/L004Window.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class L004Window : EditorWindow
+{
+	static L004Window instance = null;
+
+	public static void OpenWindow()
+	{
+		if (instance == null)
+		{
+			instance = (L004Window)ScriptableObject.CreateInstance<L004Window>();
+			instance.position = new Rect(500, 300, 600, 400);
+		}
+		instance.ShowPopup();
+		instance.Focus();
+	}
+
+	void Update()
+	{
+		if (EditorWindow.focusedWindow != this) this.Close();
+	}
+}
